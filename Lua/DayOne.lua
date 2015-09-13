27399a108476fa5dabad349b2b3430c7020030be
Jordan Robinson <j.robinson@building-blocks.com>
2015-09-13 12:59:53 +0100

    Further updates for day one of lua

--- Lua/DayOne.lua
+++ Lua/DayOne.lua
@@ -14,4 +14,20 @@ print(ends_in_3(103))
 print(ends_in_3(34683))
 print(ends_in_3(4))
 
+function is_prime(num)
+	if num <= 1 or (num ~= 2 and num % 2 == 0) then
+		return false
+	end
+
+	for i = 3, math.sqrt(num), 2 do
+		if num % i == 0 then 
+			return false
+		end
+	end
+end
+
+print(is_prime(33))
+print(is_prime(103))
+print(is_prime(34683))
+print(is_prime(4))
 
