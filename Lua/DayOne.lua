1ee1d53acea81aaebe280656e0c65d90af441f6b
Jordan Robinson <hypahnova@gmail.com>
2015-09-14 22:11:29 +0100

    More work on day one of lua

--- Lua/DayOne.lua
+++ Lua/DayOne.lua
@@ -9,10 +9,18 @@ function ends_in_3(num)
 	return false
 end
 
+print '----'
 print(ends_in_3(33))
 print(ends_in_3(103))
 print(ends_in_3(34683))
 print(ends_in_3(4))
+
+for i = 1, 100, 1 do
+	if (ends_in_3(i)) then
+		print(i)
+	end
+end
+print '----'
 
 function is_prime(num)
 	if num <= 1 or (num ~= 2 and num % 2 == 0) then
@@ -27,8 +35,36 @@ function is_prime(num)
 	return true
 end
 
+print '----'
 print(is_prime(33))
 print(is_prime(103))
 print(is_prime(34683))
 print(is_prime(4))
+
+for i = 1, 100, 1 do
+	if (is_prime(i)) then
+		print(i)
+	end
+end
+print '----'
+
+
+function first_primes_ending_in_3(n)
+x = 0
+i = 0
+
+	while x < n do
+
+		i = i + 1
+
+		if (is_prime(i) and ends_in_3(i)) then
+			print(i)
+			x = x + 1
+		end
+	end
+end
+
+
+print(first_primes_ending_in_3(100))
+print '----'
 
